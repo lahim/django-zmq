956bcc29de033094b8630d6435f8788c1e0c4602
Michał Krawczak <michal@open8.co>
2019-09-11 00:21:54 +0200

    django-zmq-3 Add a beat for scheduled tasks

--- djangozmq/settings.py
+++ djangozmq/settings.py
@@ -4,8 +4,12 @@ LOGGER_NAME = 'django-zmq'
 
 DEFAULT_PULL_SOCKET = 'tcp://127.0.0.1:5000'
 DEFAULT_PUSH_SOCKET = 'tcp://127.0.0.1:5001'
+DEFAULT_BEAT_SLEEP_TIME = 60 * 1  # value in seconds
 
 PULL_SOCKET = getattr(django_settings, 'ZMQ_PULL_SOCKET', DEFAULT_PULL_SOCKET)
 PUSH_SOCKET = getattr(django_settings, 'ZMQ_PUSH_SOCKET', DEFAULT_PUSH_SOCKET)
 
+BEAT_SLEEP_TIME = getattr(django_settings, 'ZMQ_BEAT_SLEEP_TIME', DEFAULT_BEAT_SLEEP_TIME)
+
 TASKS = getattr(django_settings, 'ZMQ_TASKS', [])
+PERIODIC_TASKS = getattr(django_settings, 'ZMQ_PERIODIC_TASKS', [])
